4ba097fc9d1ce163af2b04d2c763bfc1f8a2f59e
phantom42 <phantom42@gmail.com>
2025-12-23 21:07:34 -0500

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
+import cors from 'cors';
 import { fileURLToPath } from "url";
 import rootRouter from "./routes/root.js";
 import mythRouter from './routes/api/myths.js';
@@ -10,7 +11,7 @@ const app = express();
 //const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); 
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", rootRouter);   
 app.use("/myths", mythRouter);
