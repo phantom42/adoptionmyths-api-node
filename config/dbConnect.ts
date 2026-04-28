286a23fd01cb1338162f39a3c472092b62c9fa48
phantom42 <phantom42@gmail.com>
2026-04-28 14:15:54 -0400

    typescript fix

--- config/dbConnect.ts
+++ config/dbConnect.ts
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+	const uri = process.env.DATABASE_URI;
+	if (!uri) throw new Error('DATABASE_URI not defined');
   try {
-    await mongoose.connect(process.env.DATABASE_URI);
-    console.log("Connected to MongoDB");
+	await mongoose.connect(uri);
+	console.log("Connected to MongoDB");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+	console.error("Error connecting to MongoDB:", error);
   }
 };
 
